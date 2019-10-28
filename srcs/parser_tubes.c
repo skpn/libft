0c160b8a6294601c9fba26aa907a19f13f15a333
skpn <simon.kpenou@edu.emlyon.com>
2019-10-28 15:59:06 +0100

    corrected

--- srcs/parser_tubes.c
+++ srcs/parser_tubes.c
@@ -10,7 +10,7 @@ int		add_links(t_lem *lem, char *name1, char *name2)
 	int		ctr;
 
 	elem = lem->rooms;
-	stop = 0;
+	ctr = 0;
 	while (elem && ctr < 2)
 	{
 		room = (t_room *)elem->content;
@@ -21,8 +21,8 @@ int		add_links(t_lem *lem, char *name1, char *name2)
 	}
 	if (ctr != 2)
 		return (0);
-	ft_lstadd(room1->children, room2);
-	ft_lstadd(room2->children, room1);
+	ft_lstadd_new(&(room1->children), room2, sizeof(room1));
+	ft_lstadd_new(&(room2->children), room1, sizeof(room2));
 	return (1);
 }
 
@@ -36,11 +36,11 @@ int		parse_tube(t_lem *lem, char *line, int *index)
 	(void)index;
 	if (*line == '#')
 		return (line[1] == '#' ? 0 : 1);
-	if ((pos1 = ft_strchr_pos(line, '-')) < 1 || name1 = ft_strsub(line, 0,
-		pos1 - 1))
+	if ((pos1 = ft_strchr_pos(line, '-')) < 1 || !(name1 = ft_strsub(line, 0,
+		pos1 - 1)))
 		return (0);
-	if ((pos2 = ft_strchr_pos(line, '\n')) < 3 || name2 = ft_strsub(line, pos1 +
-		1, pos2 -1))
+	if ((pos2 = ft_strchr_pos(line, '\n')) < 3 || !(name2 = ft_strsub(line, pos1 +
+		1, pos2 -1)))
 		return (0);
 	if ((ft_strchr_pos(name2, '-')) > -1)
 		return (0);
